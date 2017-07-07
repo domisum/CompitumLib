40051402b8d8fe9a87b9b05e33764383ece69e67
Dominik <dominik.weissenseel@gmx.de>
2017-07-07 23:16:11 +0200

    Changed == to #equals() (NavMeshTriangle)

--- src/main/java/de/domisum/lib/compitum/navmesh/geometry/NavMeshTriangle.java
+++ src/main/java/de/domisum/lib/compitum/navmesh/geometry/NavMeshTriangle.java
@@ -10,6 +10,7 @@ import org.bukkit.Location;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 
 public class NavMeshTriangle
 {
@@ -152,7 +153,7 @@ public class NavMeshTriangle
 		if(this.neighbors.containsKey(other))
 		{
 			NavMeshTriangleTransition currentTransition = this.neighbors.get(other);
-			if(currentTransition == transition)
+			if(Objects.equals(currentTransition, transition))
 				CompitumLib.getLogger()
 						.warning("Overridden NavMesh neighbor: "+currentTransition+"' overridden by '"+transition+"'");
 		}
